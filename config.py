74c33e33632bd65f1e2ed581f6c3392a2454bd46
nouhaila <nohaila.chkhonty@gmail.com>
2024-12-18 14:45:39 +0100

    adding some configuration

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 import os
 
 MYSQL_CONFIG = {
-    'host': os.getenv('MYSQL_HOST', 'localhost'),  # Default to localhost if not set
+    'host': os.getenv('MYSQL_HOST', '178.16.129.132'),  # Default to localhost if not set
     'user': os.getenv('MYSQL_USER', 'root'),  # Default user as root if not set
-    'password': os.getenv('MYSQL_PASSWORD', 'nano@password'),  # Default empty password if not set
+    'password': os.getenv('MYSQL_PASSWORD', ''),  # Default empty password if not set
     'database': os.getenv('MYSQL_DATABASE', 'my_db'),  # Default DB name
-    'port': int(os.getenv('MYSQL_PORT', 3306))  # Default to port 3306 if not set
+    'port': int(os.getenv('MYSQL_PORT', 3307))  # Default to port 3306 if not set
 }
 
